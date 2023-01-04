2d3dbc4da5733f1f1ab8ae419d4a9866487480e1
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2023-01-04 13:49:21 +0600

    Initial Commit

---
+++ weather_ui/weather_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_uiApp.swift
+//  weather_ui
+//
+//  Created by Farhan Sadik on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct weather_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
